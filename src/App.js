d00534da75cdc1206b1aa6fc079cc72a42424467
mehmet dogan <mehmetseymen91@gmail.com>
2024-05-12 23:11:34 +0300

    asyn await

--- src/App.js
+++ src/App.js
@@ -6,6 +6,27 @@ import './App.css';
 
 
 function App() {
+  const [selectedImage,setSelectedImage] = useState(null);
+  const [textResult,setTextResult] = useState("");
+
+  const worker = createWorker();
+
+  const convertImageToText = async () => {
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data } = await worker.recognize(selectedImage);
+    console.log(data);
+    await worker.terminate();
+    setTextResult(data.text);
+  }
+  useEffect(()=>{
+    convertImageToText();
+  },[selectedImage])
+
+  const handleChangeImage = e =>{
+    setSelectedImage(e.target.files[0])  
+  }
   return (
     <div className="App">
 
